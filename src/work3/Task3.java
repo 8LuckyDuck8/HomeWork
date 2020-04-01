2661f712d9cd69e65d0a1556c2a76fda2bc6bd5a
LuckyDuck <sasha.talalaev2001@gmail.com>
2020-04-01 16:22:03 +0300

    Added task

--- src/work3/Task3.java
+++ src/work3/Task3.java
@@ -1,9 +1,17 @@
 package work3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
-        int a = 679769;
-        if ( a % 10 == 7)
+        Scanner in =  new Scanner(System.in);
+        System.out.print("Введите число: ");
+        int s = in.nextInt();
+        int b = 0;
+        if(s < 0){
+            b = s * (-1);
+        }
+        if ( b % 10 == 7)
             System.out.println("Последняя цифра числа является семеркой");
          else
             System.out.println("Последняя цифра числа не является семеркой");
