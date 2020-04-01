8b0e3526fa45f75d76d83864787693db8760b7ae
LuckyDuck <sasha.talalaev2001@gmail.com>
2020-04-01 15:26:53 +0300

    Fixed task

--- src/work4/Task4.java
+++ src/work4/Task4.java
@@ -5,12 +5,16 @@ import java.util.Scanner;
 public class Task4 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        
         System.out.println("Введите перую сторону: ");
         int a = in.nextInt();
+
         System.out.println("Введите вторую сторону: ");
         int b = in.nextInt();
+
         System.out.println("Введите радиус окружности: ");
         int r = in.nextInt();
+
         if(((r*2)*(r*2)) >= (a*a + b*b))
             System.out.println("Окружности заданного радиуса хватит , чтобы закрыть отверстие ");
          else
