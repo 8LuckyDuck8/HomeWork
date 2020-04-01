12d8b9707ad42f3ad34434d5a092d55ec4dba29b
LuckyDuck <sasha.talalaev2001@gmail.com>
2020-04-01 16:12:39 +0300

    Fixed task

--- src/work8/Task8.java
+++ src/work8/Task8.java
@@ -1,12 +1,23 @@
 package work8;
 
+import java.util.Scanner;
+
 public class Task8 {
     public static void main(String[] args) {
         int sum = 1 ;
-        int i = 1 ;
-        while ( i <= 10){
-            sum *=  i ;
-            i++;
+        int m = 1;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите число, факториал которого нужно посчитать [1] - [30]:");
+        int i = in.nextInt();
+        while(true){
+            if(i < 1 || i > 30){
+                System.out.println("Введите число, факториал которого нужно посчитать [1] - [30], большее число нельзя вычислить:");
+                i = in.nextInt();
+            }else break;
+        }
+        while ( m <= i){
+            sum *=  m ;
+            m++;
         }
         System.out.println(sum);
     }
