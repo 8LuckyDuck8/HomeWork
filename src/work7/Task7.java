5e0bf8231351898efa8ea6e1b28cf01dc23b8a39
LuckyDuck <sasha.talalaev2001@gmail.com>
2020-04-04 14:31:43 +0300

    Added task

--- src/work7/Task7.java
+++ src/work7/Task7.java
@@ -1,5 +1,34 @@
 package work7;
 
-public class Task7 {
+import java.util.Scanner;
 
+public class Task7 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите рзамеры участка: ");
+        int e = in.nextInt();
+        int f = in.nextInt();
+        System.out.println("Введите размеры первого дома: ");
+        int a = in.nextInt();
+        int b = in.nextInt();
+        System.out.println("Введите размеры второго дома: ");
+        int c = in.nextInt();
+        int d = in.nextInt();
+        int max_house = 0;
+        int max_place = 0;
+
+        if ((a * b + c * d) < (e * f)) {
+            if ((a + d) < e & (b + c) < f || (a + c) < e & (b + d) < f){
+                System.out.println("Поместиться");
+            }else {
+                if((a + d) < f & (b + c) < e || (a + c) < f & (b + d) < e){
+                    System.out.println("Поместиться");
+                }else {
+                    System.out.println("Не поместиться");
+                }
+            }
+        }else {
+            System.out.println("Не поместиться");
+        }
+    }
 }
