474fb60d19dbb0d0bc6e45db0bbc4c9f5cb26eee
LuckyDuck <sasha.talalaev2001@gmail.com>
2020-04-04 15:20:08 +0300

    Added task

--- src/work20/Task20.java
+++ src/work20/Task20.java
@@ -1,5 +1,21 @@
 package work20;
 
 public class Task20 {
+    public static void main(String[] args) {
+        String str = "  Тестовая   строка   разными  знаками   препинания ";
+        String str1 = "";
+        int n = 0;
+        char symbol;
+        for (int i = 0; i < str.length(); i++) {
+            symbol = str.charAt(i);
+            if (symbol != ' ') {
+                if (str.charAt(i + 1) == ' ') {
+                   str1 += symbol;
+                }
+            }
+        }
+        System.out.println(str);
+        System.out.println("Строка составленная из последних букв исходной строки: " + str1);
+    }
 
 }
