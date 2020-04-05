af137e380d0e82465eaab75a05a81484de6bf1d9
LuckyDuck <sasha.talalaev2001@gmail.com>
2020-04-05 18:54:35 +0300

    Fixed task

--- src/work18/Task18.java
+++ src/work18/Task18.java
@@ -2,12 +2,12 @@ package work18;
 
 public class Task18 {
     public static void main(String[] args) {
-        String str = "Тестовая; : ? строка. , - с разными . , знаками , . препинания";
+        String str = "test! test? test.";
         int n = 0;
         char symbol;
         for(int i = 0 ; i < str.length() ; i++){
             symbol = str.charAt(i);
-            if(symbol == ',' || symbol =='?' || symbol =='.' || symbol ==':' || symbol ==';' || symbol =='-'){
+            if(symbol == ',' || symbol =='?' || symbol =='.' || symbol ==':' || symbol ==';' || symbol =='-' || symbol == '!'){
                 n++;
             }
         }
