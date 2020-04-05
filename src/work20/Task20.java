da995684ddc370139353b3717758bb29b8d90849
LuckyDuck <sasha.talalaev2001@gmail.com>
2020-04-05 18:53:31 +0300

    Fixed task

--- src/work20/Task20.java
+++ src/work20/Task20.java
@@ -2,15 +2,24 @@ package work20;
 
 public class Task20 {
     public static void main(String[] args) {
-        String str = "  Тестовая   строка   разными  знаками   препинания ";
+        String str = " test! test ? test.., ";
         String str1 = "";
         int n = 0;
         char symbol;
         for (int i = 0; i < str.length(); i++) {
             symbol = str.charAt(i);
+            boolean b = symbol != ',' && symbol != '?' && symbol != '.' && symbol != ':' && symbol != ';' && symbol != '-' && symbol != '!';
             if (symbol != ' ') {
-                if (str.charAt(i + 1) == ' ') {
-                   str1 += symbol;
+                if(b) {
+                    if (i + 1 == str.length()) {
+                        str1 += symbol;
+                    } else {
+                        boolean b2 = str.charAt(i + 1) == ' ' || str.charAt(i + 1) == ',' || str.charAt(i + 1) =='?' || str.charAt(i + 1) =='.'
+                                || str.charAt(i + 1) ==':' || str.charAt(i + 1) ==';' || str.charAt(i + 1) =='-' || str.charAt(i + 1) == '!';
+                        if (b2) {
+                            str1 += symbol;
+                        }
+                    }
                 }
             }
         }
