4d23e9bbbbb1a579765d3979d92a0c18edcee6c2
LuckyDuck <sasha.talalaev2001@gmail.com>
2020-04-05 18:53:59 +0300

    Fixed task

--- src/work19/Task19.java
+++ src/work19/Task19.java
@@ -1,22 +1,27 @@
 package work19;
 
+import java.sql.SQLOutput;
+
 public class Task19 {
     public static void main(String[] args) {
-        String str = "  Тестовая   строка   разными  знаками   препинания  saga  asgasg  asgsag     asgasg  ";
+        String str = "  . ; : ! ? test    ?  ";
         int n = 0;
         char symbol;
         for (int i = 0; i < str.length(); i++) {
             symbol = str.charAt(i);
-            if(i < 1){
+            boolean b = symbol != ',' && symbol != '?' && symbol != '.' && symbol != ':' && symbol != ';' && symbol != '-' && symbol != '!';
+            if(i == 0 ){
                 if (symbol != ' ') {
-                    n++;
+                    if(b) {
+                        n++;
+                    }else continue;
                 }
             }else{
                 if (symbol != ' ') {
-                    if(str.charAt(i-1) != ' ') {
-                        continue;
-                    }else {
-                        n++;
+                    if (b) {
+                        if (str.charAt(i - 1) == ' ') {
+                            n++;
+                        }
                     }
                 }
 
@@ -24,5 +29,6 @@ public class Task19 {
 
         }
         System.out.println("В строке " + n + " слов");
+        System.out.println(str.charAt(0));
     }
 }
