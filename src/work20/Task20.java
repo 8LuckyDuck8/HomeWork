885660c8a84d25df17f96a6d5fe2dd4c317d6878
LuckyDuck <sasha.talalaev2001@gmail.com>
2020-04-07 15:16:09 +0300

    Fixed task

--- src/work20/Task20.java
+++ src/work20/Task20.java
@@ -2,28 +2,25 @@ package work20;
 
 public class Task20 {
     public static void main(String[] args) {
-        String str = " test! test ? test.., ";
+        String str = " test! test ? test.., ygd";
+        System.out.println(str);
+        str = str.replaceAll("\\p{P}", " ");
         String str1 = "";
         int n = 0;
         char symbol;
         for (int i = 0; i < str.length(); i++) {
             symbol = str.charAt(i);
-            boolean b = symbol != ',' && symbol != '?' && symbol != '.' && symbol != ':' && symbol != ';' && symbol != '-' && symbol != '!';
             if (symbol != ' ') {
-                if(b) {
-                    if (i + 1 == str.length()) {
+                if (i + 1 == str.length()) {
+                    str1 += symbol;
+                } else {
+                    if(str.charAt(i+1)==' ') {
                         str1 += symbol;
-                    } else {
-                        boolean b2 = str.charAt(i + 1) == ' ' || str.charAt(i + 1) == ',' || str.charAt(i + 1) =='?' || str.charAt(i + 1) =='.'
-                                || str.charAt(i + 1) ==':' || str.charAt(i + 1) ==';' || str.charAt(i + 1) =='-' || str.charAt(i + 1) == '!';
-                        if (b2) {
-                            str1 += symbol;
-                        }
                     }
                 }
             }
+
         }
-        System.out.println(str);
         System.out.println("Строка составленная из последних букв исходной строки: " + str1);
     }
 
