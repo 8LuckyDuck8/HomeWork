71ba448b3ee13bdab4938c9c8756247624aab109
LuckyDuck <sasha.talalaev2001@gmail.com>
2020-04-07 15:56:27 +0300

    Fixed task

--- src/work18/Task18.java
+++ src/work18/Task18.java
@@ -1,16 +1,14 @@
 package work18;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Task18 {
     public static void main(String[] args) {
-        String str = "test! test? test.";
+        String str = "test! test? test.*%# ? ";
+        Matcher m = Pattern.compile("\\p{Punct}").matcher(str);
         int n = 0;
-        char symbol;
-        for(int i = 0 ; i < str.length() ; i++){
-            symbol = str.charAt(i);
-            if(symbol == ',' || symbol =='?' || symbol =='.' || symbol ==':' || symbol ==';' || symbol =='-' || symbol == '!'){
-                n++;
-            }
-        }
-        System.out.println("В строке "+ n + " знаков препинаяния");
+        while(m.find()) n++;
+        System.out.println("В строке " + n + " знаков препинаяния");
     }
 }
