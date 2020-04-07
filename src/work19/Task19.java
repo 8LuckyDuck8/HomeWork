dad61932f47e8f217c2dbc4a226a5ff237fe69b8
LuckyDuck <sasha.talalaev2001@gmail.com>
2020-04-07 14:46:24 +0300

    Added task

--- src/work19/Task19.java
+++ src/work19/Task19.java
@@ -4,31 +4,30 @@ import java.sql.SQLOutput;
 
 public class Task19 {
     public static void main(String[] args) {
-        String str = "  . ; : ! ? test    ?  ";
+        String str = "   asgag  . ;sagasg : !asgas ? test    ? sagasgas asgf ";
+        str = str.replaceAll("\\p{P}", " ");
         int n = 0;
         char symbol;
         for (int i = 0; i < str.length(); i++) {
             symbol = str.charAt(i);
-            boolean b = symbol != ',' && symbol != '?' && symbol != '.' && symbol != ':' && symbol != ';' && symbol != '-' && symbol != '!';
-            if(i == 0 ){
+            if (i == 0) {
                 if (symbol != ' ') {
-                    if(b) {
-                        n++;
-                    }else continue;
+
+                    n++;
                 }
-            }else{
+            } else {
                 if (symbol != ' ') {
-                    if (b) {
-                        if (str.charAt(i - 1) == ' ') {
-                            n++;
-                        }
+                    if (str.charAt(i - 1) == ' ') {
+                        n++;
                     }
                 }
-
             }
 
         }
+
+
         System.out.println("В строке " + n + " слов");
-        System.out.println(str.charAt(0));
     }
 }
+
+
